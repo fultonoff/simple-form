890ccb604337d984ed477e905d7b4d33f689bdd6
fulton <fultonkilebe7@gmail.com>
2021-03-18 18:37:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.burger')
+const navMenu = document.querySelector('.nav__menu')
+
+
+burger.addEventListener('click', (e)=>{
+
+    burger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
